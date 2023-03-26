88a08355e36e0d380e7efad0a97e8dfb7012b3be
korayademoglu <korayademoglu@gmail.com>
2023-03-26 16:38:16 +0300

    Componen Mantigi

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "bulma/css/bulma.css";
+import "./App.css";
+import Course from "./Course";
+import Angular from "./images/angular.jpg";
+import Boostrap from "./images/bootstrap5.png";
+import Csharp from "./images/ccsharp.png";
+import KompleWeb from "./images/kompleweb.jpg";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const test ="Test Koray";
+  const test = "Test Koray";
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">Kurslar</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column">
+              <Course
+                img={Angular}
+                title="Angular"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio necessitatibus ea amet totam recusandae commodi dolores repudiandae, ipsa magni non consequatur labore repellendus, similique odio blanditiis vero. Nihil, maiores pariatur."
+              />
+            </div>
+            <div className="column">
+              <Course
+                img={Boostrap}
+                title="Boostrap 5"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio necessitatibus ea amet totam recusandae commodi dolores repudiandae, ipsa magni non consequatur labore repellendus, similique odio blanditiis vero. Nihil, maiores pariatur."
+              />
+            </div>
+            <div className="column">
+              <Course
+                img={Csharp}
+                title="Backend"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio necessitatibus ea amet totam recusandae commodi dolores repudiandae, ipsa magni non consequatur labore repellendus, similique odio blanditiis vero. Nihil, maiores pariatur."
+              />
+            </div>
+            <div className="column">
+              <Course
+                img={KompleWeb}
+                title="Komple Web"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio necessitatibus ea amet totam recusandae commodi dolores repudiandae, ipsa magni non consequatur labore repellendus, similique odio blanditiis vero. Nihil, maiores pariatur."
+              />
+            </div>
+          </div>
+        </section>
       </div>
-      <h1>{test}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Sayac {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Course.jsx
@@ -0,0 +1,26 @@
+function Course({ img, title, description }) {
+  // const title=props.title;
+  // const description=props.description;
+  // const {title,description} = props;
+  // console.log(props);
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src={img} alt="Kurslar" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="title is-4">{title}</p>
+          </div>
+        </div>
+
+        <div className="content">{description}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Course;
